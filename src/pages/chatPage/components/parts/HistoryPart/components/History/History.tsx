50322bfabcc0f6aca7b547cc513289f2c1ca88ad
Islam Madaminov <isxmad@gmail.com>
2023-08-28 15:14:33 +0500

    feat: компонент элемента истории

--- src/pages/chatPage/components/parts/HistoryPart/components/History/History.tsx
+++ src/pages/chatPage/components/parts/HistoryPart/components/History/History.tsx
@@ -1,9 +1,47 @@
 import styles from './History.module.scss';
 import { FC } from 'react'
+import Avatar from '@components/Avatar/Avatar';
+import UserTitle from '@components/UserTitle/UserTitle';
+import CopyableText from '@components/CopyableText/CopyableText';
 
-const History:FC<any> = () => {
+interface I {
+  man?: any,
+  girl?:any,
+  message?:any,
+  operator?:any,
+}
+
+const History:FC<I> = ({
+  man,
+  girl,
+  message,
+  operator
+}) => {
   return (
-    <div className={styles.wrapper}></div>
+    <div className={styles.wrapper}>
+      <div className={styles.part}>
+        <div className={styles.title}>
+          <UserTitle name='Username' age={20}/>
+        </div>
+        <div className={styles.avatar}>
+          <Avatar isOnline/>
+        </div>
+        <div className={styles.id}>id<CopyableText>1232</CopyableText></div>
+      </div>
+      <div className={styles.body}>
+        <div className={styles.message}>some message</div>
+        <div className={styles.operator}></div>
+      </div>
+      <div className={styles.part}>
+        <div className={styles.title}>
+          <UserTitle name='Username' age={20}/>
+        </div>
+        <div className={styles.avatar}>
+          <Avatar isOnline/>
+        </div>
+        <div className={styles.id}>id<CopyableText>1232</CopyableText></div>
+      </div>
+    </div>
   )
 }
 
